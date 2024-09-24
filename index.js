289ee61cc2d74e5a66837ecca77e6c28da56ed4b
hamim <hamim2114@gmail.com>
2024-09-24 23:21:15 +0600

    updated

--- index.js
+++ index.js
@@ -44,15 +44,14 @@ app.use(cors({
     'https://edu-quest-admin.vercel.app',
     'https://edu-quest-silk.vercel.app'
   ],
-  credentials: true
+  // credentials: true
 }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allowing all origins
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
