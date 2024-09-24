fefd443171bcc1f7e5224879a4bb0061b00ccc9d
hamim <hamim2114@gmail.com>
2024-09-24 22:55:17 +0600

    updated

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import multer from 'multer';
 import { blogRoute } from './route/blog.route.js';
 import { handleDelete, handleUpload } from './utils/fileUploadHandler.js';
 import { userRoute } from './route/user.route.js';
+import corsOptions from './config/corsOptions.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -37,24 +38,8 @@ app.listen(PORT, () => {
   connectToDatabase();
 });
 
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:4000',
-    'https://edu-quest-admin.vercel.app',
-    'https://edu-quest-silk.vercel.app'
-  ],
-  credentials: true
-}));
+app.use(cors(corsOptions));
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allowing all origins
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
 
 // File upload configuration
 const storage = multer.memoryStorage();
