90b75b904f282f6f58bb913655dc14d4898e0672
hamim <hamim2114@gmail.com>
2024-09-25 20:02:59 +0600

    updated

--- index.js
+++ index.js
@@ -56,24 +56,16 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-    res.send('cors problem fixed:)');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
 });
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', req.headers.origin);
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   res.header('Access-Control-Allow-Credentials', true);
-//   next();
-// });
 
 // Routes
 app.use('/api/auth', userRoute);
