1f3f74a1629f2570805be52f278b6b81f0c6d140
hamim <hamim2114@gmail.com>
2024-09-25 16:20:29 +0600

    updated

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow specific origin
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 const corsOptions = {
   origin: ['localhost:3000', 'https://edu-quest-silk.vercel.app'], // Allow localhost and production frontends
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
