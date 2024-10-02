63d06026d49ce79435ac3bd900945fddb8f8484a
hamim <hamim2114@gmail.com>
2024-10-02 17:17:12 +0600

    updated

--- index.js
+++ index.js
@@ -52,17 +52,6 @@ app.use(cors({origin: [
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-
 // File upload configuration
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
